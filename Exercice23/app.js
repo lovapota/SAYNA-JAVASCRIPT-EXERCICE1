35d9db87f132b3dba816db797ecbdb3d27e08456
lovapota <rfenosoa1@gmail.com>
2022-05-03 11:37:19 -0700

    Finalisation de l'exercice

--- Exercice23/app.js
+++ Exercice23/app.js
@@ -74,11 +74,11 @@ button.addEventListener('click', (e) => {
 })
 // 4.	Construisez un quiz de cinq questions.
 let question = [
-    'Quelle science etudie les atomes et les molecules',
-    'La force d attraction de la lune est responsable, sur Terre',
-    'Quel est le point d ebullition de l eau',
-    'Combien de fois par jour inspirons nous?',
-    'Par quelle unite de mesure estime t on la valeur d un diamant',
+    '1-Quelle science etudie les atomes et les molecules',
+    '2-La force d attraction de la lune est responsable, sur Terre',
+    '3-Quel est le point d ebullition de l eau',
+    '4-Combien de fois par jour inspirons nous?',
+    '5-Par quelle unite de mesure estime t on la valeur d un diamant',
 ]
 let reponse = [
     {A : 'A.La chimie', B : 'B.La physique', C : 'C.La biologie'},
@@ -87,11 +87,17 @@ let reponse = [
     {A : 'A.20 000 fois', B : 'B.200 000 fois', C : 'C.2 000 C'},
     {A : 'A.Le carat', B : 'B.Le varat', C : 'C.Le barat'},
 ]
+let izy = ['La chimie', 'des 4 saisons', '100 C', '20 000 fois', 'Le carat'];
 
 let quizz = document.createElement('form');
 document.body.appendChild(quizz);
+let envoyer = document.createElement('button');
+envoyer.textContent = 'Resultat';
+quizz.appendChild(envoyer);
+let safidy1 = [];
+let safidy2 = [];
+let safidy3 = [];
 for(let i = 0; i < question.length; i ++){
-    
     let fanotaniana = document.createElement('p');
     let choix1 = document.createElement('input');
     let choix2 = document.createElement('input');
@@ -99,29 +105,54 @@ for(let i = 0; i < question.length; i ++){
     let label1 = document.createElement('label');
     let label2 = document.createElement('label');
     let label3 = document.createElement('label');
+    quizz.appendChild(fanotaniana);
     quizz.appendChild(choix1);
     quizz.appendChild(label1);
     quizz.appendChild(choix2);
     quizz.appendChild(label2);
     quizz.appendChild(choix3);
     quizz.appendChild(label3);
-    choix1.setAttribute("type","checkbox");
-    choix2.setAttribute("type","checkbox");
-    choix3.setAttribute("type","checkbox");
-    quizz.appendChild(fanotaniana);
+    choix1.setAttribute("type","radio");
+    choix1.setAttribute("name", `question${i}`);
+    choix2.setAttribute("type","radio");
+    choix2.setAttribute("name", `question${i}`);
+    choix3.setAttribute("type","radio");
+    choix3.setAttribute("name", `question${i}`);
     fanotaniana.textContent = question[i];
     label1.textContent = `${reponse[i].A}`;
     label2.textContent = `${reponse[i].B}`;
-    label3.textContent = `${reponse[i].C}`;
-    
-}
-
-
-
-
-
-// Exigences :
-// ●	Créez un formulaire HTML avec un quiz de cinq questions.
-// ●	Chaque question doit comporter quatre réponses à choix multiple.
-// ●	Lorsque l'utilisateur soumet le quiz, ajoutez un message sous chaque question pour lui faire savoir s'il a bien ou mal répondu (donnez-lui la bonne réponse).
-// ●	À la fin du quiz, affichez le résultat eb. 2 correct / sur 5
+    label3.textContent = `${reponse[i].C}`; 
+    choix1.addEventListener('click', () => {
+        safidy1.push(1);
+        safidy2.push(0);
+        safidy3.push(0);
+    })
+    choix2.addEventListener('click', () => {
+        safidy1.push(0);
+        safidy2.push(1);
+        safidy3.push(0);
+    })
+    choix3.addEventListener('click', () => {
+        safidy1.push(0);
+        safidy2.push(0);
+        safidy3.push(1);
+    })
+};
+envoyer.addEventListener('click', (e) => {
+    let valiny = document.createElement('h2');
+    document.body.appendChild(valiny);
+    let iza =[];
+   
+    for(let j = 1 ; j <= 5; j ++){
+        let textV = document.createElement('h3');
+        document.body.appendChild(textV);
+        textV.textContent = `${j} - ${izy[j]}`;
+        if(safidy1[j] === 1){
+            iza.push(2);   
+        }
+    };
+    console.log(iza);
+    console.log(safidy1);
+    valiny.textContent = `Le résultat est de ${iza.length} corrects / sur 5`;
+    e.preventDefault();
+})
